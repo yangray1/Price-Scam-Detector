2ad722f7127a3f13cc40fe74757726e396e851e7
Raymond Yang <raaymond.yang@mail.utoronto.ca>
2019-08-10 20:01:27 -0400

    Completed walmart scraper

--- app/src/main/java/com/example/antiscamdetector/Webscraper.java
+++ app/src/main/java/com/example/antiscamdetector/Webscraper.java
@@ -111,24 +111,77 @@ public class Webscraper {
         return products;
     }
 
+    public ArrayList<Product> scrapeWalmart(String targetTitleForUrl, String originalTargetTitle)
+            throws IOException{
+
+        // Search the first page of amazon search.
+        ArrayList<Product> products = new ArrayList<>();
+        String url ="https://www.walmart.com/search/?cat_id=0&query=" + targetTitleForUrl;
+        String itemSelector = ".search-result-gridview-items > .Grid-col"; // Individual Product boxes
+
+        Document page = Jsoup.connect(url).userAgent("Jsoup Scraper").get();
+        Elements items = page.select(itemSelector);
+
+        String productTitle;
+        String productPrice = "";
+        String productUrl;
+        String productImageSrc;
+
+        String titleSelector = ".product-title-link"; // title attr
+        String priceSelector = ".price-main > .visuallyhidden";
+        String urlSelector = ".product-title-link"; // href attr.
+        String imageSrcSelector = "img";
+
+        for (Element item:items){
+
+            // Get the first name if there's multiple names.
+            productTitle = item.select(titleSelector).attr("title");
+
+            // Check if the price is a single price or a price range.
+            Elements temp = item.select(priceSelector);
+            if (temp.text().length() > 0){
+                productPrice = temp.text();
+                if (temp.size() == 2){
+                    productPrice = temp.first().text() + " - " + temp.last().text();
+                }
+            }
+
+            // Do not include priceless items or items with names that does not include targetTitle.
+            if (!productTitle.toLowerCase().contains(originalTargetTitle) || productPrice.equals("")){
+                continue;
+            }
+            productUrl = "https://walmart.com" + item.select(urlSelector).attr("href");
+            productImageSrc =  item.select(imageSrcSelector).attr("data-image-src");
+
+            Product product = new Product(productTitle, productPrice, productUrl, productImageSrc);
+            products.add(product);
+        }
+        return products;
+    }
     public static void main(String[] args) throws IOException {
 
-        String targetTitleForUrl = "yugioh+cards";
-        String originalTargetTitle = "yugioh cards";
+        String targetTitleForUrl = "league+of+legends";
+        String originalTargetTitle = "league of legends";
         Webscraper Webscraper = new Webscraper();
 
         /* Tests for scraping Ebay */
-        // ArrayList<Product> products = Webscraper.scrapeEbay( "league+of+legends", "league of legends");
-        // ArrayList<Product> products = Webscraper.scrapeEbay("yu-gi-oh", "yu-gi-oh");
-        // ArrayList<Product> products = Webscraper.scrapeEbay("FDW+CLEAR+SPRINGS+125+GMS", " ");
+//         ArrayList<Product> products = Webscraper.scrapeEbay( "league+of+legends", "league of legends");
+//         ArrayList<Product> products = Webscraper.scrapeEbay("yu-gi-oh", "yu-gi-oh");
+//         ArrayList<Product> products = Webscraper.scrapeEbay("FDW+CLEAR+SPRINGS+125+GMS", " ");
 
         /* Tests for scraping Amazon */
-        // ArrayList<Product> products2 = Webscraper.scrapeAmazon("league+of+legends", "league of legends");
-        // ArrayList<Product> products2 = Webscraper.scrapeAmazon("yu-gi-oh", "yu-gi-oh");
-        // ArrayList<Product> products2 = Webscraper.scrapeAmazon("FDW+CLEAR+SPRINGS+125+GMS", " ");
+//         ArrayList<Product> products2 = Webscraper.scrapeAmazon("league+of+legends", "league of legends");
+//         ArrayList<Product> products2 = Webscraper.scrapeAmazon("yu-gi-oh", "yu-gi-oh");
+//         ArrayList<Product> products2 = Webscraper.scrapeAmazon("FDW+CLEAR+SPRINGS+125+GMS", " ");
+
+        /* Tests for scraping Walmart */
+//         ArrayList<Product> products3 = Webscraper.scrapeWalmart("league+of+legends", "league of legends");
+//         ArrayList<Product> products3 = Webscraper.scrapeWalmart("yu-gi-oh", "yu-gi-oh");
+//         ArrayList<Product> products3 = Webscraper.scrapeWalmart("FDW+CLEAR+SPRINGS+125+GMS", " ");
 
         ArrayList<Product> products = Webscraper.scrapeEbay(targetTitleForUrl, originalTargetTitle);
         ArrayList<Product> products2 = Webscraper.scrapeAmazon(targetTitleForUrl, originalTargetTitle);
+        ArrayList<Product> products3 = Webscraper.scrapeWalmart(targetTitleForUrl, originalTargetTitle);
 
         System.out.println("size for EBAY: " +  products.size());
         for (Product item:products){
@@ -138,6 +191,12 @@ public class Webscraper {
 
         System.out.println("size for AMAZON: " +  products2.size());
         for (Product item:products2){
+            item.print();
+            System.out.println();
+        }
+
+        System.out.println("size for WALAMRT: " +  products3.size());
+        for (Product item:products3){
             item.print();
             System.out.println();
         }
